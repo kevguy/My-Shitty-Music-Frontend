2f9cf6f218ab5bee7257444330299f94192e7555
Kev Lai <kevlai22@uw.edu>
2018-05-31 20:06:07 +0800

    chore: update server.js

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
 
+// create the express app
 const app = express()
-app.use(serveStatic(path.join(__dirname, '/dist')))
-
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log('built server started ' + port))
+// create middleware to handle the serving the app
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+// Create default port to serve the app on
+const port = process.env.PORT || 5000
+app.listen(port)
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port)
