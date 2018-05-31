9e91c053dc9712c67502f2f99471c727ef64965e
Kev Lai <kevlai22@uw.edu>
2018-05-31 19:48:05 +0800

    production build

---
+++ dist/precache-manifest.a1ab0ffcc47118f738d905c3dd2d7955.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54c29b7a0a055db81c8a",
+    "url": "/js/vendors~app.54c29b7a.js"
+  },
+  {
+    "revision": "a52bbe0c8e9e37b66570",
+    "url": "/js/app.a52bbe0c.js"
+  },
+  {
+    "revision": "f9e4e21a6b5977cfe117c6856dd9fa6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54c29b7a0a055db81c8a",
+    "url": "/css/vendors~app.0.3636a09b.css"
+  },
+  {
+    "revision": "a52bbe0c8e9e37b66570",
+    "url": "/css/app.6b6c9bc9.css"
+  }
+];
